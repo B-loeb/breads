f7262ea7378f105e858f8270f398887b67b3eefc
B-loeb <bradleyloeb@gmail.com>
2022-07-19 19:52:22 -0400

    show route

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -6,5 +6,9 @@ const Bread = require('../models/bread.js')
 breads.get('/', (req, res) => {
   res.send(Bread)
 })
-
+// SHOW
+breads.get('/:arrayIndex', (req, res) => {
+    res.send(Bread[req.params.arrayIndex])
+  })
+  
 module.exports = breads
