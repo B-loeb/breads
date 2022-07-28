aa3c5d2c7d7ae690d5f333915df7301ac30b08aa
B-loeb <bradleyloeb@gmail.com>
2022-07-28 19:24:27 -0400

    BreadCrud Schema

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // DEPENDENCIES
+const mongoose = require('mongoose')
 const express = require('express')
 
 // CONFIGURATION
@@ -6,17 +7,18 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 //MiddleWare must be above ROUTES
-
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+
 // ROUTES
 app.get('/', (req, res) => {
   res.send('House of 1000 Breads')
 })
-
-
 
 //Breads
 const breadsController = require('./controllers/breads_controller.js')
